5ad1ba82614dcc09be9e0e5bca853c7f738342b3
DefinitelyNotMLG <72935141+DefinitelyNotMLG@users.noreply.github.com>
2020-12-31 16:56:32 -0500

    create api

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from "./API"
 //components
-import QuestionCard from "./components/questionCard";
+import QuestionCard from "./components/QuestionCard";
+//Types
+import { Difficulty } from "./API";
+
+const Total_Questions = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(Total_Questions, Difficulty.EASY))
+
+
   const startTrivia = async () => {
 
 
@@ -21,7 +36,14 @@ const App = () => {
       <button className="start" onClick={startTrivia}>Start</button>
       <p className="score">Score:</p>
       <p>Loading Questions</p>
-      <QuestionCard />
+      {/*<QuestionCard
+        questionNr={number + 1}
+        totalQuestions={Total_Questions}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswer[number] : undefined}
+        callback={checkAnswer}
+      />*/}
       <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
