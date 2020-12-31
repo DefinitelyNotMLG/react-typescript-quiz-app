50d920da97ff698ddafd89358351e7ccd74b0cce
DefinitelyNotMLG <72935141+DefinitelyNotMLG@users.noreply.github.com>
2020-12-31 17:36:36 -0500

    compleate utils.ts

--- my-app/src/API.ts
+++ my-app/src/API.ts
@@ -1,23 +1,30 @@
+import { shuffleArray } from "./utils"
+
 
 export type question = {
-catagory: string;
-correct_answer: string;
-difficulty: string;
-incorect_answer: string[];
-question: string;
-type: string;
+    catagory: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
 };
 
 export type questionState = question & { answers: string[] };
 
 export enum Difficulty {
-    EASY ="easy",
-    MEDIUM ="medium",
-    HARD ="hard",
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
 }
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
+    return data.results.map((question: question) => (
+        {
+            ...question,
+            answer: shuffleArray([...question.incorrect_answer, question.correct_answer,])
+        }
+    ))
 }
